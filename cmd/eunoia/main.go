f9f1fc58a3064cf696e8641c60c38e61c7bb65aa
Master Struct <masterstruct@proton.me>
2026-07-21 21:16:33 +0300

    Add missing import and init bitboards

--- cmd/eunoia/main.go
+++ cmd/eunoia/main.go
@@ -2,7 +2,13 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/masterstruct/Eunoia/internal/board"
 )
+
+func init() {
+	board.InitBitboards()
+}
 
 func main() {
 	fmt.Println("Eunoia chess engine")
