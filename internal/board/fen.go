e36282ef5c16a275f7844c677e6be54f8fc65373
Master Struct <masterstruct@proton.me>
2026-07-27 15:17:33 +0300

    Implement basic ParseFEN func

--- internal/board/fen.go
+++ internal/board/fen.go
@@ -1,5 +1,71 @@
 package board
 
+import (
+	"strconv"
+	"strings"
+)
+
 func ParseFEN(fen string) (Position, error) {
-	return NewPosition(), nil
+	pos := NewPosition()
+	file := FileA
+	rank := Rank8
+	splits := strings.Split(fen, " ")
+	n := len(splits)
+
+	if n >= 1 {
+		for _, char := range splits[0] {
+			if char == '/' {
+				file = 0
+				rank -= 1
+				continue
+			}
+			skip, err := strconv.Atoi(string(char))
+			if err == nil && !(skip < 1 || skip > 8) {
+				file += skip
+				continue
+			} // TODO: handle error
+
+			pt := ParsePieceType(byte(char))
+			color := NoColor
+			if pt != NoPieceType {
+				// char is a chess piece
+				if char&0x20 != 0 {
+					// uppercase => Black
+					color = Black
+				} else {
+					color = White
+				}
+				pos.PlacePiece(NewPiece(pt, color), NewSquare(file, rank))
+				file += 1
+				continue
+			}
+		}
+	}
+	if n >= 2 {
+		pos.SideToMove = ParseColor(splits[1][0])
+	}
+	if n >= 3 {
+		// TODO: handle error
+		rights, _ := ParseCastlingRights(splits[2])
+		pos.CastlingRights = rights
+	}
+	if n >= 4 {
+		// TODO: handle error
+		sq, _ := ParseSquare(splits[3])
+		pos.EnPassant = sq
+	}
+	if n >= 5 {
+		halfMove, err := strconv.Atoi(splits[4])
+		if err == nil {
+			pos.HalfmoveClock = uint8(halfMove)
+		} // TODO: handle error
+	}
+	if n >= 6 {
+		ply, err := strconv.Atoi(splits[5])
+		if err == nil {
+			pos.Ply = uint16(ply)
+		} // TODO: handle error
+	}
+
+	return pos, nil
 }
