60e972683a3f9a4d597defff5a619a1095053690
Master Struct <masterstruct@proton.me>
2026-07-29 17:06:26 +0300

    Add flag consts and credit 87flowers

--- internal/board/move.go
+++ internal/board/move.go
@@ -1,3 +1,24 @@
 package board
 
+// Thank you, 87flowers, for
+// your chess move flag scheme
+// https://87flowers.com/chess-moveflags/
+
 type Move uint16
+
+const (
+	flagQuiet           uint8 = 0b0000
+	flagDoublePush      uint8 = 0b0001
+	flagCastleQueenside uint8 = 0b0010
+	flagCastleKingside  uint8 = 0b0011
+
+	flagCapture   uint8 = 0b1000
+	flagEnPassant uint8 = 0b1001
+
+	flagPromoKnight uint8 = 0b0100
+	flagPromoBishop uint8 = 0b0101
+	flagPromoRook   uint8 = 0b0110
+	flagPromoQueen  uint8 = 0b0111
+)
+
+const NullMove = 0 // from A1 to A1 - impossible!
