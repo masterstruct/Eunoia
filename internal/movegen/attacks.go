1eb5350a1cd58400263a146575bb81c4c3ab5aa1
Master Struct <masterstruct@proton.me>
2026-08-07 23:31:44 +0300

    Declare Knight, King and Pawn attack tables

--- internal/movegen/attacks.go
+++ internal/movegen/attacks.go
@@ -1,1 +1,9 @@
 package movegen
+
+import "github.com/masterstruct/Eunoia/internal/board"
+
+var (
+	KnightAttacks [64]board.Bitboard
+	KingAttacks   [64]board.Bitboard
+	PawnAttacks   [2][64]board.Bitboard // indexed by board.Color
+)
