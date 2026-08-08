2e0dba9580d6cecad4bc6cd1a582dfa8648adb1b
Master Struct <masterstruct@proton.me>
2026-08-08 12:59:35 +0300

    'Do I have ADHD' refactor part 2

--- internal/movegen/sliders.go
+++ internal/movegen/sliders.go
@@ -3,8 +3,16 @@ package movegen
 import "github.com/masterstruct/Eunoia/internal/board"
 
 var (
-	rookDirs   = [][2]int{{1, 0}, {-1, 0}, {0, 1}, {0, -1}}
-	bishopDirs = [][2]int{{1, 1}, {1, -1}, {-1, 1}, {-1, -1}}
+	rookDirs = [][2]int{
+		/*     */ {0, 1},
+		{-1, 0} /*  ♜ */, {1, 0},
+		/*     */ {0, -1},
+	}
+	bishopDirs = [][2]int{
+		{-1, 1}, {1, 1},
+		/*      ♝     */
+		{-1, -1}, {1, -1},
+	}
 )
 
 func RookAttacks(sq board.Square, occupied board.Bitboard) board.Bitboard {
