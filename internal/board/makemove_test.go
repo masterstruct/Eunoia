d7a9511198d0c4dbce8a1a56d7822b576c58820e
Master Struct <masterstruct@proton.me>
2026-08-08 22:23:56 +0300

    Add more tests

--- internal/board/makemove_test.go
+++ internal/board/makemove_test.go
@@ -123,7 +123,96 @@ func TestMakeMove(t *testing.T) {
 			move:    NewMove(H1, H2),
 			wantFEN: "8/8/8/8/8/8/7k/4K3 w - - 1 8",
 		},
-		// TODO: add more tests
+		{
+			name:    "black quiet move clears en passant",
+			fen:     "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1",
+			move:    NewMove(G8, F6),
+			wantFEN: "rnbqkb1r/pppppppp/5n2/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 1 2",
+		},
+		{
+			name:    "black captures en passant, removes correct pawn",
+			fen:     "4k3/8/8/8/3pP3/8/8/4K3 b - e3 0 1",
+			move:    NewEnPassant(D4, E3),
+			wantFEN: "4k3/8/8/8/8/4p3/8/4K3 w - - 0 2",
+		},
+		{
+			name:    "any move clears stale en passant, not just the capturing pawn",
+			fen:     "4k3/8/8/2Pp4/8/8/8/4K3 w - d6 0 1",
+			move:    NewMove(E1, E2),
+			wantFEN: "4k3/8/8/2Pp4/8/8/4K3/8 b - - 1 1",
+		},
+		{
+			name:    "black kingside castle moves rook too",
+			fen:     "4k2r/8/8/8/8/8/8/4K3 b k - 0 1",
+			move:    NewCastle(E8, G8, true),
+			wantFEN: "5rk1/8/8/8/8/8/8/4K3 w - - 1 2",
+		},
+		{
+			name:    "black queenside castle moves rook too",
+			fen:     "r3k3/8/8/8/8/8/8/4K3 b q - 0 1",
+			move:    NewCastle(E8, C8, false),
+			wantFEN: "2kr4/8/8/8/8/8/8/4K3 w - - 1 2",
+		},
+		{
+			name:    "white king move clears KQ only, kq untouched",
+			fen:     "r3k2r/8/8/8/8/8/8/R3K2R w KQkq - 0 1",
+			move:    NewMove(E1, E2),
+			wantFEN: "r3k2r/8/8/8/8/8/4K3/R6R b kq - 1 1",
+		},
+		{
+			name:    "black king move clears kq only, KQ untouched",
+			fen:     "r3k2r/8/8/8/8/8/8/R3K2R b KQkq - 0 1",
+			move:    NewMove(E8, E7),
+			wantFEN: "r6r/4k3/8/8/8/8/8/R3K2R w KQ - 1 2",
+		},
+		{
+			name:    "black kingside rook move clears k only, Qq untouched",
+			fen:     "r3k2r/8/8/8/8/8/8/R3K2R b KQkq - 0 1",
+			move:    NewMove(H8, H7),
+			wantFEN: "r3k3/7r/8/8/8/8/8/R3K2R w KQq - 1 2",
+		},
+		{
+			name:    "black queenside rook move clears q only, Kk untouched",
+			fen:     "r3k2r/8/8/8/8/8/8/R3K2R b KQkq - 0 1",
+			move:    NewMove(A8, A6),
+			wantFEN: "4k2r/8/r7/8/8/8/8/R3K2R w KQk - 1 2",
+		},
+		{
+			name:    "black captures white rook on h1, clears K via captured-square not mover-square",
+			fen:     "4k3/8/8/8/7r/8/8/R3K2R b KQkq - 0 1",
+			move:    NewCapture(H4, H1),
+			wantFEN: "4k3/8/8/8/8/8/8/R3K2r w Qkq - 0 2",
+		},
+		{
+			name:    "rook move from non-home square doesn't touch castling rights",
+			fen:     "4k3/8/8/8/8/8/8/R2RK2R w KQ - 0 1",
+			move:    NewMove(D1, D5),
+			wantFEN: "4k3/8/8/3R4/8/8/8/R3K2R b KQ - 1 1",
+		},
+		{
+			name:    "capturing a rook off its home square doesn't touch castling rights",
+			fen:     "4k3/8/8/3r4/1N6/8/8/R3K2R w KQ - 0 1",
+			move:    NewCapture(B4, D5),
+			wantFEN: "4k3/8/8/3N4/8/8/8/R3K2R b KQ - 0 1",
+		},
+		{
+			name:    "underpromotion places knight, not queen",
+			fen:     "8/4P3/8/8/8/8/8/4K2k w - - 12 5",
+			move:    NewPromo(E7, E8, Knight),
+			wantFEN: "4N3/8/8/8/8/8/8/4K2k b - - 0 5",
+		},
+		{
+			name:    "black promotion places correct piece",
+			fen:     "4K2k/8/8/8/8/8/4p3/8 b - - 0 1",
+			move:    NewPromo(E2, E1, Rook),
+			wantFEN: "4K2k/8/8/8/8/8/8/4r3 w - - 0 2",
+		},
+		{
+			name:    "black capture promotion removes victim, places promoted piece",
+			fen:     "4K2k/8/8/8/8/8/4p3/3N4 b - - 0 1",
+			move:    NewCapturePromo(E2, D1, Queen),
+			wantFEN: "4K2k/8/8/8/8/8/8/3q4 w - - 0 2",
+		},
 	}
 
 	for _, tt := range tests {
