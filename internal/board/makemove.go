7953c80caca29f5ff773e6337d9fb995bbe985d1
Master Struct <masterstruct@proton.me>
2026-08-08 16:15:11 +0300

    Add failing MakeMove method

--- internal/board/makemove.go
+++ internal/board/makemove.go
@@ -1,6 +1,21 @@
 package board
 
-func (pos Position) MakeMove(m Move) Position {
+func (pos Position) MakeMove(move Move) Position {
 	newPos := pos
+	from, to := move.From(), move.To()
+	piece, _ := pos.PieceOn(from)
+
+	// move/capture piece
+	newPos.RemovePiece(to)
+	newPos.PlacePiece(pos.Board[from], to)
+	newPos.RemovePiece(from)
+
+	// update
+	newPos.SideToMove = pos.SideToMove.Opponent()
+	newPos.Ply++
+	if move.IsCapture() || piece.Type == Pawn {
+		newPos.HalfmoveClock = 0
+	}
+
 	return newPos
 }
