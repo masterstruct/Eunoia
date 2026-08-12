cce8d7fcaa817ac92b2ce06ce1540f72b7802bba
Master Struct <masterstruct@proton.me>
2026-08-12 17:40:13 +0300

    Implement IsSquareAttacked func

--- internal/movegen/movegen.go
+++ internal/movegen/movegen.go
@@ -1,7 +1,27 @@
 package movegen
 
-import "github.com/masterstruct/Eunoia/internal/board"
+import (
+	"github.com/masterstruct/Eunoia/internal/board"
+)
 
 func IsSquareAttacked(pos board.Position, sq board.Square, byColor board.Color) bool {
+	if KnightAttacks[sq]&pos.PieceBB(board.NewPiece(board.Knight, byColor)) != 0 {
+		return true
+	}
+	if KingAttacks[sq]&pos.PieceBB(board.NewPiece(board.King, byColor)) != 0 {
+		return true
+	}
+	if PawnAttacks[byColor.Opponent()][sq]&pos.PieceBB(board.NewPiece(board.Pawn, byColor)) != 0 {
+		return true
+	}
+	if RookAttacks(sq, pos.Occupied())&pos.PieceBB(board.NewPiece(board.Rook, byColor)) != 0 {
+		return true
+	}
+	if BishopAttacks(sq, pos.Occupied())&pos.PieceBB(board.NewPiece(board.Bishop, byColor)) != 0 {
+		return true
+	}
+	if QueenAttacks(sq, pos.Occupied())&pos.PieceBB(board.NewPiece(board.Queen, byColor)) != 0 {
+		return true
+	}
 	return false
 }
