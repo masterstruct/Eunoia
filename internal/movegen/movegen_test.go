5a2eecf2b795a629f527e308b7e33e94bc70a3ab
Master Struct <masterstruct@proton.me>
2026-08-13 15:59:43 +0300

    Add GenQueenMoves test

--- internal/movegen/movegen_test.go
+++ internal/movegen/movegen_test.go
@@ -284,6 +284,51 @@ func TestGenRookMoves(t *testing.T) {
 	}
 }
 
+func TestGenQueenMoves(t *testing.T) {
+	tests := []struct {
+		name string
+		fen  string
+		to   []board.Square
+	}{
+		{
+			name: "starting position for white has 0 queen moves",
+			fen:  board.StartingFEN,
+			to:   []board.Square{},
+		},
+		{
+			name: "starting position for black has 0 queen moves",
+			fen:  "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR b KQkq - 0 1",
+			to:   []board.Square{},
+		},
+		{
+			name: "kiwipete for white has 9 queen moves",
+			fen:  "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -",
+			to: []board.Square{board.D3, board.E3, board.F4, board.F5,
+				board.F6, board.G3, board.G4, board.H3, board.H5},
+		},
+		{
+			name: "kiwipete for black has 4 queen moves",
+			fen:  "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R b KQkq -",
+			to:   []board.Square{board.C5, board.D6, board.D8, board.F8},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			pos, _ := board.ParseFEN(tt.fen)
+
+			movelist := genQueenMoves(pos)
+			for _, move := range movelist {
+				if !slices.Contains(tt.to, move.To()) {
+					t.Fatalf("unexpected queen move: %v\n%v", move, pos)
+				}
+			}
+			if len(movelist) != len(tt.to) {
+				t.Fatalf("missing moves: expected %v but got %v\n%v", tt.to, movelist, pos)
+			}
+		})
+	}
+}
+
 // TODO: figure out castling pseudolegal checks
 
 // func TestGenKingMoves(t *testing.T) {
