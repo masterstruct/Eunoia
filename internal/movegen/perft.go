4bc19609a5ab553ba268fb2f0a397d9cee9275b8
Master Struct <masterstruct@proton.me>
2026-08-14 14:26:10 +0300

    Implement SplitPerft func

--- internal/movegen/perft.go
+++ internal/movegen/perft.go
@@ -1,6 +1,10 @@
 package movegen
 
-import "github.com/masterstruct/Eunoia/internal/board"
+import (
+	"fmt"
+
+	"github.com/masterstruct/Eunoia/internal/board"
+)
 
 func Perft(pos board.Position, depth int) uint64 {
 	if depth == 0 {
@@ -16,4 +20,25 @@ func Perft(pos board.Position, depth int) uint64 {
 		}
 	}
 	return nodes
+}
+
+func SplitPerft(pos board.Position, depth int) uint64 {
+	if depth == 0 {
+		return 1
+	}
+
+	var nodes uint64
+	var total uint64
+	movelist := GeneratePseudolegalMoves(pos)
+	for _, move := range movelist {
+		newPos := pos.MakeMove(move)
+		if InCheck(newPos) {
+			continue
+		}
+		nodes = Perft(newPos, depth-1)
+		fmt.Println(move, nodes)
+		total += nodes
+	}
+	fmt.Println("Total:", total)
+	return total
 }
