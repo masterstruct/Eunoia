200373186ad480f7f9d1128af0bf07452673586c
Master Struct <masterstruct@proton.me>
2026-08-14 14:14:45 +0300

    Implement Perft function

--- internal/movegen/perft.go
+++ internal/movegen/perft.go
@@ -1,1 +1,19 @@
 package movegen
+
+import "github.com/masterstruct/Eunoia/internal/board"
+
+func Perft(pos board.Position, depth int) uint64 {
+	if depth == 0 {
+		return 1
+	}
+
+	var nodes uint64
+	movelist := GeneratePseudolegalMoves(pos)
+	for _, move := range movelist {
+		newPos := pos.MakeMove(move)
+		if !InCheck(newPos) {
+			nodes += Perft(newPos, depth-1)
+		}
+	}
+	return nodes
+}
