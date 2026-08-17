c74550b042cb863294e6b9f9e4a4eeaf9ece7b2d
Master Struct <masterstruct@proton.me>
2026-08-17 23:23:36 +0300

    Init magic moves on startup

--- cmd/genmagics/main.go
+++ cmd/genmagics/main.go
@@ -23,35 +23,11 @@ func findMagic(isRook bool, sq board.Square, indexBits uint8, rng *rand.Rand) (m
 		magic := rng.Uint64() & rng.Uint64() & rng.Uint64()
 		entry := movegen.MagicEntry{Mask: mask, Magic: magic, IndexBits: indexBits}
 
-		table, ok := tryMakeTable(isRook, sq, &entry)
+		table, ok := movegen.TryMakeTable(isRook, sq, &entry)
 		if ok {
 			return entry, table
 		}
 	}
-}
-
-func tryMakeTable(isRook bool, sq board.Square, entry *movegen.MagicEntry) ([]board.Bitboard, bool) {
-	tableSize := 1 << entry.IndexBits
-	table := make([]board.Bitboard, tableSize)
-	used := make([]bool, tableSize)
-
-	var moves board.Bitboard
-	for blockers := range movegen.Subsets(entry.Mask) {
-		if isRook {
-			moves = movegen.RookAttacks(sq, blockers)
-		} else {
-			moves = movegen.BishopAttacks(sq, blockers)
-		}
-
-		index := movegen.MagicIndex(entry, blockers)
-		if !used[index] {
-			used[index] = true
-			table[index] = moves
-		} else if table[index] != moves {
-			return []board.Bitboard{}, false
-		}
-	}
-	return table, true
 }
 
 func writeMagics(name string, magics *[64]movegen.MagicEntry, w io.Writer) {
--- internal/movegen/magics.go
+++ internal/movegen/magics.go
@@ -1,6 +1,10 @@
 package movegen
 
-import "github.com/masterstruct/Eunoia/internal/board"
+import (
+	"fmt"
+
+	"github.com/masterstruct/Eunoia/internal/board"
+)
 
 func RookMask(sq board.Square) board.Bitboard {
 	file := sq.File()
@@ -27,6 +31,7 @@ func BishopMask(sq board.Square) board.Bitboard {
 
 var RookMagics [64]MagicEntry
 var BishopMagics [64]MagicEntry
+
 var RookMoves [64][]board.Bitboard
 var BishopMoves [64][]board.Bitboard
 
@@ -56,4 +61,45 @@ func Subsets(mask board.Bitboard) func(yield func(board.Bitboard) bool) {
 			subset = (subset - 1) & mask
 		}
 	}
+}
+
+func init() {
+	initMagics(true, &RookMagics, &RookMoves, "rook")
+	initMagics(false, &BishopMagics, &BishopMoves, "bishop")
+}
+
+func initMagics(isRook bool, magics *[64]MagicEntry, moves *[64][]board.Bitboard, name string) {
+	for sq := board.A1; sq <= board.H8; sq++ {
+		entry := &magics[sq]
+
+		table, ok := TryMakeTable(isRook, sq, entry)
+		if !ok {
+			panic(fmt.Sprintf("Bad %s magics! Square %v", name, sq))
+		}
+		moves[sq] = table
+	}
+}
+
+func TryMakeTable(isRook bool, sq board.Square, entry *MagicEntry) ([]board.Bitboard, bool) {
+	tableSize := 1 << entry.IndexBits
+	table := make([]board.Bitboard, tableSize)
+	used := make([]bool, tableSize)
+
+	var moves board.Bitboard
+	for blockers := range Subsets(entry.Mask) {
+		if isRook {
+			moves = RookAttacks(sq, blockers)
+		} else {
+			moves = BishopAttacks(sq, blockers)
+		}
+
+		index := MagicIndex(entry, blockers)
+		if !used[index] {
+			used[index] = true
+			table[index] = moves
+		} else if table[index] != moves {
+			return []board.Bitboard{}, false
+		}
+	}
+	return table, true
 }
