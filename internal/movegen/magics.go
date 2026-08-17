1c4ae649dc74425acc184861159bbc95f5f2cb39
Master Struct <masterstruct@proton.me>
2026-08-17 19:58:43 +0300

    Implement rookMask func

--- internal/movegen/magics.go
+++ internal/movegen/magics.go
@@ -3,5 +3,18 @@ package movegen
 import "github.com/masterstruct/Eunoia/internal/board"
 
 func rookMask(sq board.Square) board.Bitboard {
-	return 0
+	file := sq.File()
+	rank := sq.Rank()
+	var mask board.Bitboard
+
+	for f := board.FileB; f < board.FileH; f++ {
+		mask.SetBit(board.NewSquare(f, rank))
+	}
+
+	for r := board.Rank2; r < board.Rank8; r++ {
+		mask.SetBit(board.NewSquare(file, r))
+	}
+
+	mask.ClearBit(sq)
+	return mask
 }
