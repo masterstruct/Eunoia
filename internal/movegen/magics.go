539fd1450c199ed33924eb3c3668f9f23ae8136e
Master Struct <masterstruct@proton.me>
2026-08-17 19:53:46 +0300

    Add magics.go and failing RookMask test

---
+++ internal/movegen/magics.go
@@ -0,0 +1,7 @@
+package movegen
+
+import "github.com/masterstruct/Eunoia/internal/board"
+
+func rookMask(sq board.Square) board.Bitboard {
+	return 0
+}
---
+++ internal/movegen/magics_test.go
@@ -0,0 +1,41 @@
+package movegen
+
+import (
+	"testing"
+
+	"github.com/masterstruct/Eunoia/internal/board"
+)
+
+func TestRookMask(t *testing.T) {
+	var tests = []struct {
+		sq   board.Square
+		want board.Bitboard
+	}{
+		{
+			sq:   board.A1,
+			want: 0x101010101017e,
+		},
+		{
+			sq:   board.G2,
+			want: 0x40404040403e00,
+		},
+		{
+			sq:   board.D4,
+			want: 0x8080876080800,
+		},
+		{
+			sq:   board.H8,
+			want: 0x7e80808080808000,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.sq.String(), func(t *testing.T) {
+			got := rookMask(tt.sq)
+
+			if got != tt.want {
+				t.Errorf("expected %v, but got %v", tt.want, got)
+			}
+		})
+	}
+}
