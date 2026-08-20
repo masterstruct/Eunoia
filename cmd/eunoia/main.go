4e28fcea529da004ec3cec96401e299116ef0afe
Master Struct <masterstruct@proton.me>
2026-08-20 16:43:38 +0300

    Remove context from main.go

--- cmd/eunoia/main.go
+++ cmd/eunoia/main.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-	"context"
 	"os"
-	"os/signal"
 
 	"github.com/masterstruct/Eunoia/internal/uci"
 )
 
 func main() {
-	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
-	defer stop()
-
-	uci.Loop(ctx, os.Stdin, os.Stdout)
+	uci.Loop(os.Stdin, os.Stdout)
 }
