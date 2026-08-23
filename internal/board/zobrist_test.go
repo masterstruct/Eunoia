d3a3d15e723d1a959e900c70527a56d2706928e8
Master Struct <masterstruct@proton.me>
2026-08-23 17:29:16 +0300

    Add PRNG reproducibility test

--- internal/board/zobrist_test.go
+++ internal/board/zobrist_test.go
@@ -20,3 +20,16 @@ func TestPRNGNext(t *testing.T) {
 		}
 	}
 }
+
+func TestPRNGNext_Reproducible(t *testing.T) {
+	p1 := NewPRNG(zobristSeed)
+	p2 := NewPRNG(zobristSeed)
+
+	for i := range 5 {
+		got1 := p1.Next()
+		got2 := p2.Next()
+		if got1 != got2 {
+			t.Fatalf("call %d: sequences don't match: %d vs %d", i+1, got1, got2)
+		}
+	}
+}
