0f50dc5840bdc9456dd6d03e0672194f8578e363
Master Struct <masterstruct@proton.me>
2026-09-01 16:30:04 +0300

    Add aspiration windows bench: 2118673

--- internal/search/search.go
+++ internal/search/search.go
@@ -15,22 +15,45 @@ const (
 func (ss *SearchState) SearchBestMove(pos board.Position, maxDepth int) board.Move {
 	var bestMove board.Move
 
+	var lastScore int16
+	aw := newAspirationWindow() // [-INF; +INF]
+
 	// iterative deepening
+
+iterativeDeepening:
 	for depth := 1; depth <= maxDepth; depth++ {
 		if (ss.SoftNodes > 0 && ss.Nodes >= ss.SoftNodes) || (!ss.SoftTime.IsZero() && time.Now().After(ss.SoftTime)) {
 			break
 		}
 
-		score := ss.negamax(pos, depth, 0, -INF, INF)
-
-		if len(ss.pv.Line()) == 0 {
-			// interruped before first move search completed,
-			// use bestMove from previous depth
-			break
+		if depth > 1 {
+			aw.centerAround(lastScore)
 		}
 
-		bestMove = ss.pv.Line()[0]
-		ss.printPV(os.Stdout, depth, score)
+		// aspiration search
+		for {
+			score := ss.negamax(pos, depth, 0, aw.alpha, aw.beta)
+
+			if len(ss.pv.Line()) == 0 {
+				// interruped before first move search completed,
+				// discard results from this depth
+				break iterativeDeepening
+			}
+
+			if score <= aw.alpha {
+				aw.widenDown()
+				continue
+			}
+			if score >= aw.beta {
+				aw.widenUp()
+				continue
+			}
+
+			bestMove = ss.pv.Line()[0]
+			ss.printPV(os.Stdout, depth, score)
+			lastScore = score
+			break
+		}
 
 		if ss.searchStopped() {
 			break
