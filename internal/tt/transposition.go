2ffaf0e6e90d50d817d0a9dae0b73d51c1ef5c22
Master Struct <masterstruct@proton.me>
2026-09-11 22:19:14 +0300

    Implement variable size TT

--- internal/tt/transposition.go
+++ internal/tt/transposition.go
@@ -23,23 +23,44 @@ type Entry struct {
 	Flag  Flag
 }
 
-// TODO: variable size TT
-const Size uint = 1 << 22 // 64MiB
-const Mask uint = Size - 1
+const DefaultSizeMiB uint = 64
 
-type Table [Size]Entry
+type Table struct {
+	entries []Entry
+	mask    uint64
+}
+
+func NewTable(sizeMiB uint) *Table {
+	if sizeMiB == 0 {
+		sizeMiB = DefaultSizeMiB
+	}
+	size := sizeFromMiB(sizeMiB)
+	return &Table{
+		entries: make([]Entry, size),
+		mask:    uint64(size - 1),
+	}
+}
+
+func (tt *Table) Resize(sizeMiB uint) {
+	if sizeMiB == 0 {
+		sizeMiB = DefaultSizeMiB
+	}
+	size := sizeFromMiB(sizeMiB)
+	tt.entries = make([]Entry, size)
+	tt.mask = uint64(size - 1)
+}
 
 func (tt *Table) Clear() {
 	if tt != nil {
-		*tt = Table{}
+		clear(tt.entries)
 	}
 }
 
 func (tt *Table) Store(key uint64, move board.Move, score int16, depth uint8, flag Flag) {
-	if tt == nil {
+	if tt == nil || len(tt.entries) == 0 {
 		return
 	}
-	entry := &tt[tt.index(key)]
+	entry := &tt.entries[tt.index(key)]
 	if entry.Key != key || depth >= entry.Depth {
 		entry.Key = key
 		entry.Move = move
@@ -50,15 +71,15 @@ func (tt *Table) Store(key uint64, move board.Move, score int16, depth uint8, fl
 }
 
 func (tt *Table) Probe(key uint64) (Entry, bool) {
-	if tt == nil {
+	if tt == nil || len(tt.entries) == 0 {
 		return Entry{}, false
 	}
-	entry := tt[tt.index(key)]
+	entry := tt.entries[tt.index(key)]
 	return entry, entry.Key == key
 }
 
-func (t *Table) index(key uint64) uint64 {
-	return key & uint64(Mask)
+func (tt *Table) index(key uint64) uint64 {
+	return key & tt.mask
 }
 
 func sizeFromMiB(mb uint) uint {
