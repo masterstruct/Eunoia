30dacd18afba3fc1e757f88a512fecdfdaf54cbf
Master Struct <masterstruct@proton.me>
2026-09-14 21:09:47 +0300

    Add insufficient material detection bench: 5617662

--- internal/search/repetition.go
+++ internal/search/repetition.go
@@ -3,7 +3,7 @@ package search
 import "github.com/masterstruct/Eunoia/internal/board"
 
 func (ss *SearchState) isDraw(pos *board.Position) bool {
-	return pos.HalfmoveClock >= 100 || ss.isRepetition(pos.Hash, pos.HalfmoveClock)
+	return pos.HalfmoveClock >= 100 || isInsufficientMaterial(pos) || ss.isRepetition(pos.Hash, pos.HalfmoveClock)
 }
 
 func (ss *SearchState) isRepetition(hash uint64, halfmoveClock uint8) bool {
@@ -27,4 +27,22 @@ func (ss *SearchState) isRepetition(hash uint64, halfmoveClock uint8) bool {
 		}
 	}
 	return false
+}
+
+func isInsufficientMaterial(pos *board.Position) bool {
+	pawns := pos.Pieces[board.Pawn]
+	rooks := pos.Pieces[board.Rook]
+	queens := pos.Pieces[board.Queen]
+	if (pawns | rooks | queens) != 0 {
+		// KvK
+		return false
+	}
+
+	knights := pos.Pieces[board.Knight]
+	bishops := pos.Pieces[board.Bishop]
+
+	minorPieces := knights | bishops
+
+	// impossible to mate with less than 2 minor pieces
+	return minorPieces.CountBits() <= 1
 }
