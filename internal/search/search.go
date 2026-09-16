7be36c9df14f9153803607003803a846d9cbd973
Master Struct <masterstruct@proton.me>
2026-09-17 00:38:12 +0300

    Fix incomplete depth nullmove as bestmove bench: 5511851

--- internal/search/search.go
+++ internal/search/search.go
@@ -4,6 +4,7 @@ import (
 	"os"
 
 	"github.com/masterstruct/Eunoia/internal/board"
+	"github.com/masterstruct/Eunoia/internal/movegen"
 )
 
 const (
@@ -12,7 +13,10 @@ const (
 )
 
 func (ss *SearchState) SearchBestMove(pos board.Position) board.Move {
-	var bestMove board.Move
+	bestMove, ok := firstLegalMove(pos)
+	if !ok {
+		return board.NullMove
+	}
 
 	var lastScore int16
 	aw := newAspirationWindow() // [-INF; +INF]
@@ -58,4 +62,19 @@ iterativeDeepening:
 		}
 	}
 	return bestMove
+}
+
+func firstLegalMove(pos board.Position) (board.Move, bool) {
+	var movelist movegen.Movelist
+	movegen.GeneratePseudolegalMoves(&pos, &movelist)
+	mover := pos.SideToMove
+
+	for i := range movelist.Len {
+		move := movelist.Moves[i]
+		newPos := pos.MakeMove(move)
+		if !movegen.InCheck(&newPos, mover) {
+			return move, true
+		}
+	}
+	return board.NullMove, false
 }
