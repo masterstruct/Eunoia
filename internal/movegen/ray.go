1d714fbb15b1854d243e6306be363b28db31f966
Master Struct <masterstruct@proton.me>
2026-09-18 21:54:00 +0300

    Add Extending func bench: 5511851

--- internal/movegen/ray.go
+++ internal/movegen/ray.go
@@ -3,15 +3,21 @@ package movegen
 import "github.com/masterstruct/Eunoia/internal/board"
 
 var (
-	between [64][64]board.Bitboard
+	between   [64][64]board.Bitboard
+	extending [64][64]board.Bitboard
 )
 
 func Between(a, b board.Square) board.Bitboard {
 	return between[a][b]
 }
 
+func Extending(a, b board.Square) board.Bitboard {
+	return extending[a][b]
+}
+
 func init() {
 	initBetween()
+	initExtending()
 }
 
 func initBetween() {
@@ -23,6 +29,20 @@ func initBetween() {
 
 			if BishopAttacks(a, board.EmptyBB).IsBitSet(b) {
 				between[a][b] = BishopAttacks(a, board.SquareBB[b]) & BishopAttacks(b, board.SquareBB[a])
+			}
+		}
+	}
+}
+
+func initExtending() {
+	for a := range board.NoSquare {
+		for b := range board.NoSquare {
+			if RookAttacks(a, board.EmptyBB).IsBitSet(b) {
+				extending[a][b] = RookAttacks(a, board.EmptyBB) & RookAttacks(b, board.EmptyBB)
+			}
+
+			if BishopAttacks(a, board.EmptyBB).IsBitSet(b) {
+				extending[a][b] = BishopAttacks(a, board.EmptyBB) & BishopAttacks(b, board.EmptyBB)
 			}
 		}
 	}
--- internal/movegen/ray_test.go
+++ internal/movegen/ray_test.go
@@ -31,3 +31,29 @@ func TestBetween(t *testing.T) {
 		})
 	}
 }
+
+func TestExtending(t *testing.T) {
+	tests := []struct {
+		a    board.Square
+		b    board.Square
+		want board.Bitboard
+	}{
+		{board.A1, board.H8, 0x40201008040200},
+		{board.A8, board.H1, 0x2040810204000},
+		{board.D2, board.D7, 0x800080808080008},
+		{board.H3, board.D7, 0x400102040000000},
+		{board.A2, board.G2, 0xbe00},
+		{board.D2, board.A2, 0xf600},
+		{board.D4, board.E5, 0x8040200000040201},
+		{board.D4, board.D5, 0x808080000080808},
+	}
+
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			got := Extending(tt.a, tt.b)
+			if got != tt.want {
+				t.Fatalf("from %v to %v\ngot:\n%v\nwant:\n%v", tt.a, tt.b, got, tt.want)
+			}
+		})
+	}
+}
